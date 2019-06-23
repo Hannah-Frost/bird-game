3fa156023634ce177a1d41c4fb993eb03817a030
Hannah Frost <h.7frost@gmail.com>
2019-06-23 19:54:59 +0100

    Added in template background for main page

--- public/Bird.js
+++ public/Bird.js
@@ -1,7 +1,25 @@
-function Bird(colour) {
+function Bird(colour, x, y) {
+  this.leftGroundImage = ;
+  this.rightGroundImage = ;
+  this.leftJumpImage = ;
+  this.rightJumpImage = ;
   this.colour = colour;
+  this.x = x;
+  this.y = y;
+  this.speed = 5;
+  this.update = function() {
+
+  }
 }
 
 Bird.prototype.showColour = function() {
   return this.colour
+};
+
+Bird.prototype.moveLeft = function() {
+  this.x -= 5
+};
+
+Bird.prototype.moveRight = function() {
+  this.x -= 5
 };
