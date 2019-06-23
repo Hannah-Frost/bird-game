ee74ab53a0a9c984ebd678ff8b0075f764fd976f
Hannah Frost <h.7frost@gmail.com>
2019-06-23 22:11:07 +0100

    Detects when movement keys are pressed

---
+++ Bird.js
@@ -0,0 +1,25 @@
+function Bird(colour) {
+  this.leftGroundImage = "leftgroundBird.png";
+  this.rightGroundImage = "rightgroundBird.png";
+  this.leftJumpImage = "leftJumpBird.png";
+  this.rightJumpImage = "rightJumpBird.png";
+  this.colour = colour;
+  this.x = 100;
+  this.y = 100;
+  this.speed = 5;
+  this.update = function() {
+
+  }
+}
+
+Bird.prototype.showColour = function() {
+  return this.colour
+};
+
+Bird.prototype.moveLeft = function() {
+  this.x -= 5
+};
+
+Bird.prototype.moveRight = function() {
+  this.x += 5
+};
